0d578d7cec2a7a20d0af627bc08b75c500bfdf34
Dmitry <20125dimanovogrodsky@gmail.com>
2024-02-03 10:19:18 +0300

    init

---
+++ src/main/java/syberry/api/banking/BankingApplication.java
@@ -0,0 +1,13 @@
+package syberry.api.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingApplication.class, args);
+	}
+
+}
